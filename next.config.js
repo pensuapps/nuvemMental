c37ff6dbd5c995ab0b2ad491783978374d9f57fb
WillianOliveir1 <williano@id.uff.br>
2022-10-08 23:24:21 -0300

    First Deployed. PWA is not working.

--- next.config.js
+++ next.config.js
@@ -5,13 +5,18 @@
 // }
 
 // module.exports = nextConfig
-
 const withPWA = require('next-pwa')({
 		dest: 'public',
 		register: true,
-		skipWaiting: true
+		skipWaiting: true,
+		// loader: 'imgix',
 })
 
 module.exports = withPWA({
 	// next.js config
+	experimental: {
+		images: {
+			unoptimized: true,
+		},
+	},
 })
--- pages/api/_app.js
+++ pages/api/_app.js
@@ -1,7 +1,15 @@
-import '../../styles/globals.css'
-
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import '../..styles/globals.css'
+import { useEffect } from "react"
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Document>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+      <Component {...pageProps} />
+    </Document>
+  );
 }
 
 export default MyApp
