da5e33b63dbfc08844774713b7a8c544140a1a86
WillianOliveir1 <williano@id.uff.br>
2022-11-06 00:22:23 -0300

    Adjust script warning

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 class MyDocument extends Document {
 	render() {
@@ -8,8 +9,8 @@ class MyDocument extends Document {
 					<link rel="manifest" href="/manifest.json"/>
 					<link rel="apple-touch-icon" href="/brainfog-logo.png"></link>
 
-					<script async src="https://www.googletagmanager.com/gtag/js?id=G-Q67MYCWE50"></script>
-					<script
+					<Script async src="https://www.googletagmanager.com/gtag/js?id=G-Q67MYCWE50"></Script>
+					<Script
 						dangerouslySetInnerHTML={{
 							__html: `
 								window.dataLayer = window.dataLayer || [];
